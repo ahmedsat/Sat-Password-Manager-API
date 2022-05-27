c232dc2ead540f53e1b3f5b1513bf784596de013
ahmedsat <ahmedsat303@gmail.com>
2022-05-27 17:18:55 +0200

    UserSchema

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: 3,
+    maxlength: 255,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 6,
+    maxlength: 255,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
