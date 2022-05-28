49786e9a1fa913d9049cb78ee1ee54bd04459ce1
ahmedsat <ahmedsat303@gmail.com>
2022-05-28 17:04:21 +0200

    add get all users and delete user routs

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, register } = require("../controllers/auth");
+const {
+  login,
+  register,
+  deleteUser,
+  getAllUsers,
+} = require("../controllers/auth");
 
 router.post("/login", login);
 router.post("/register", register);
+
+//? this routes are only for admin we can add middleware for admin only
+router.get("/", getAllUsers);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
