85f0d2eb488a89ef014465aaf4cd9b883b3219c3
ahmedsat <ahmedsat303@gmail.com>
2022-05-28 18:07:13 +0200

    register with hashed password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -26,6 +27,12 @@ const UserSchema = new mongoose.Schema({
     enum: ["user", "admin"],
     default: "user",
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  const genSalt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, genSalt);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
