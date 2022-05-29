7b97b419477e326a1f12e82014d54482638cf844
ahmedsat <ahmedsat303@gmail.com>
2022-05-29 21:31:17 +0200

    extra security middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
 
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+
 const express = require("express");
 
 const ConnectedDB = require("./DB/connectDB");
@@ -9,11 +14,23 @@ const authRouter = require("./routes/auth");
 const passwordsRouter = require("./routes/password");
 
 const { errorHandler, notFoundHandler, auth } = require("./middlewares");
+const { default: rateLimit } = require("express-rate-limit");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.get("/", (req, res) => {
   res.json({ ...req.headers });
