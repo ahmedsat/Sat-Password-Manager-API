ebed48ab01544e33522a53b8d2270252a05d3657
ahmedsat <ahmedsat303@gmail.com>
2022-05-29 20:48:58 +0200

    ValidationError

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,18 +1,28 @@
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const ErrorHandler = (err, req, res, next) => {
-  console.log(err);
-  if (err instanceof CustomAPIError) {
-    res.status(err.statusCode).json({
-      message: err.message,
-    });
-  } else {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      message: "Internal server error",
-      error: err,
-    });
+  let CustomError = {
+    message: err.message || "Internal server error",
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+  };
+
+  if (err.name === "ValidationError") {
+    CustomError = {
+      message: Object.values(err.errors)
+        .map((error) => error.message)
+        .join(", "),
+      statusCode: StatusCodes.BAD_REQUEST,
+    };
   }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+  //   message: err,
+  //   error: err,
+  // });
+
+  return res.status(CustomError.statusCode).json({
+    message: CustomError.message,
+  });
 };
 
 module.exports = ErrorHandler;
