be4731f8f85509514811293676366bb163f6e03d
ahmedsat <ahmedsat303@gmail.com>
2022-05-30 18:09:59 +0200

    Done Fixes #4

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -16,15 +16,15 @@ const ErrorHandler = (err, req, res, next) => {
   }
 
   if (err.code && err.code === 11000) {
-    customError.msg = `Duplicate value entered for ${Object.keys(
+    CustomError.message = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`;
-    customError.statusCode = 400;
+    CustomError.statusCode = 400;
   }
 
   if (err.name === "CastError") {
-    customError.msg = `No item found with id : ${err.value}`;
-    customError.statusCode = 404;
+    CustomError.message = `No item found with id : ${err.value}`;
+    CustomError.statusCode = 404;
   }
 
   return res.status(CustomError.statusCode).json({
